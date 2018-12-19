ecb75188203fcc3acc6b398b4578224df8acaada
alekslis <alekslis@tutanota.com>
2018-12-19 21:33:45 +0100

    Create khepera.c

---
+++ khepera.c
@@ -0,0 +1,53 @@
+if czujniki us > np.30/50 odo_goto(x, y (czyli np.30,0))
+int max_us=0;
+int maxi;
+for(i=0;i<5;i++)
+{
+  usvalues=
+
+  if (usvalues[i]>max_us)
+  {
+    max_us=usvalues[i];
+    maxi=i;
+  }
+
+}
+if (maxi==0){
+  kh4_set_position(poz_l-10000,poz_r+10000,dsPic);
+  sleep(2);
+}
+else if(maxi==1){
+  kh4_set_position(poz_l-5000,poz_r+5000,dsPic);
+  sleep(2);
+}
+else if(maxi==2){
+  kh4kh4_set_position(5000,5000,dsPic);
+  sleep(2);
+}
+else if(maxi==3){
+  kh4_set_position(poz_l+5000,poz_r-5000,dsPic);
+  sleep(2);
+}
+else if(maxi==4){
+  kh4_set_position(poz_l+10000,poz_r-10000,dsPic);
+  sleep(2);
+}
+
+float gotox,gotoy;
+if (usvalues[2]>30)
+{
+  gotox=result_x+0.2;
+  gotoy=result_y;
+  odometry_goto(gotox,gotoy);
+}
+else if(usvalues[0]>30)
+{
+  kh4_set_position(poz_l-10000,poz_r+10000,dsPic);
+  odometry_goto(result_x,result_y-0.2);
+}
+else if(usvalues[4])
+{
+  kh4_set_position(poz_l+10000,poz_r-10000,dsPic);
+  odometry_goto(result_x,result_y+0.2);
+}
+kh4_set_speed(r_speed_left,r_speed_right,dsPic);
